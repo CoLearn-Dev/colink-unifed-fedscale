3fe1e83039d4311cd0b122fb6c5118e38b9f363c
xmhuangzhen <xmhuangzhen@sjtu.edu.cn>
2023-05-05 22:20:56 +0800

    update dataset

--- src/unifed/frameworks/fedscale/protocol.py
+++ src/unifed/frameworks/fedscale/protocol.py
@@ -1,176 +1,3 @@
-# import os
-# import json
-# import subprocess
-# import tempfile
-# import logging
-# from typing import List
-
-# import colink as CL
-
-# from unifed.frameworks.fedscale.util import store_error, store_return, get_local_ip
-
-# pop = CL.ProtocolOperator(__name__)
-# UNIFED_TASK_DIR = "unifed:task"
-
-
-# USE_FILE_LOGGING = True
-# if USE_FILE_LOGGING:
-#     logging.basicConfig(filename="temp.log",
-#                         filemode='a',
-#                         format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-#                         datefmt='%H:%M:%S',
-#                         level=logging.DEBUG)
-
-
-# def load_config_from_param_and_check(param: bytes):
-#     unifed_config = json.loads(param.decode())
-#     framework = unifed_config["framework"]
-#     assert framework == "fedscale"
-#     deployment = unifed_config["deployment"]
-#     if deployment["mode"] != "colink":
-#         raise ValueError("Deployment mode must be colink")
-#     return unifed_config
-
-# def convert_unifed_config_to_fedscale_config(unifed_config):  # note that for the target config, the "data" field is still missing
-#     fedscale_config = {
-#         "n_parties": len(unifed_config["deployment"]["participants"]) - 1, 
-#          "verbose": 1,
-#     }
-#     if unifed_config['algorithm'] == 'fedavg':
-#         fedscale_config["algorithm"] = "fed_avg"
-#     else:
-#         raise ValueError(f"Unknown algorithm {unifed_config['algorithm']}")
-    
-
-#     return fedscale_config
-
-
-# def create_conf_file_content_from_fedscale_config(fedscale_config):
-#     conf_file_content = ""
-#     for key, value in fedscale_config.items():
-#         conf_file_content += f"{key}={value}\n"
-#     return conf_file_content
-
-
-# def filter_log_from_fedscale_output(output):
-#     filtered_output = ""
-#     for line in output.split("\n"):
-#         line_json = json.dumps({"fedscale": line})  # TODO: convert log here
-#         if ".cpp" in line:
-#             filtered_output += f"{line_json}\n"
-#     return filtered_output
-
-
-# @pop.handle("unifed.fedscale:server")
-# @store_error(UNIFED_TASK_DIR)
-# @store_return(UNIFED_TASK_DIR)
-# def run_server(cl: CL.CoLink, param: bytes, participants: List[CL.Participant]):
-#     logging.info(f"{cl.get_task_id()[:6]} - Server - Started")
-#     print("running server start")
-#     unifed_config = load_config_from_param_and_check(param)
-#     fedscale_config = convert_unifed_config_to_fedscale_config(unifed_config)
-
-#     temp_conf_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
-#     temp_conf_file.write(create_conf_file_content_from_fedscale_config(fedscale_config))
-#     temp_conf_file.close()
-
-#     participant_id = [i for i, p in enumerate(participants) if p.user_id == cl.get_user_id()][0]
-#     process = subprocess.Popen(
-#         [
-#             "unifed-fedscale",
-#             "server",
-#             temp_conf_file.name,
-#         ],
-#         stdout=subprocess.PIPE, 
-#         stderr=subprocess.PIPE,
-#     )
-
-#     print("run_server process open")
-
-#     # make sure the server is started first before sharing IP
-#     server_ip = get_local_ip()
-#     cl.send_variable("server_ip", server_ip, [p for p in participants if p.role == "client"])
-#     # as soon as one client finishes, all clients should have finished
-#     print(' setting fariable server ip')
-#     first_client_return_code = cl.recv_variable("client_finished", [p for p in participants if p.role == "client"][0]).decode()
-#     print(' run server client finished')
-#     process.kill()
-#     stdout, stderr = process.communicate()
-#     print(' run server calculating output')
-#     log = filter_log_from_fedscale_output(stdout.decode() + stderr.decode())
-#     # TODO: store the model
-#     cl.create_entry(f"{UNIFED_TASK_DIR}:{cl.get_task_id()}:log", log)
-#     os.unlink(temp_conf_file.name)
-#     return json.dumps({
-#         "server_ip": server_ip,
-#         "stdout": stdout.decode(),
-#         "stderr": stderr.decode(),
-#         "returncode": int(first_client_return_code),
-#     })
-    
-    
-# @pop.handle("unifed.fedscale:client")
-# @store_error(UNIFED_TASK_DIR)
-# @store_return(UNIFED_TASK_DIR)
-# def run_client(cl: CL.CoLink, param: bytes, participants: List[CL.Participant]):
-#     print('run client start')
-#     logging.info(f"{cl.get_task_id()[:6]} - Client[(unknown)] - Started")
-#     server_in_list = [p for p in participants if p.role == "server"]
-#     assert len(server_in_list) == 1, f"There should be exactly one server, not {len(server_in_list)} servers ({server_in_list})."
-#     p_server = server_in_list[0]
-#     def get_client_id():
-#         passed_server = 0
-#         for i, p in enumerate(participants):
-#             if p.role == "server":
-#                 passed_server += 1
-#             if p.user_id == cl.get_user_id():
-#                 return i - passed_server
-#     client_id = get_client_id()
-#     print('run client client_id',client_id)
-#     logging.info(f"{cl.get_task_id()[:6]} - Client[{client_id}] - Recognized")
-
-#     unifed_config = load_config_from_param_and_check(param)
-#     fedscale_config = convert_unifed_config_to_fedscale_config(unifed_config)
-#     print('run client fedscale config:',fedscale_config)
-#     fedscale_config["data"] = f"./data/{unifed_config['dataset']}_{client_id}.csv"
-#     test_data_path = f"./data/{unifed_config['dataset']}"+"_test.csv"
-#     if os.path.isfile(f"{test_data_path}"):
-#         fedscale_config["test_data"] = test_data_path
-#     server_ip = fedscale_config["ip_address"] = cl.recv_variable("server_ip", p_server).decode()
-
-#     print('run_client_fedscale_config:',fedscale_config)
-
-#     temp_conf_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
-#     temp_conf_file.write(create_conf_file_content_from_fedscale_config(fedscale_config))
-#     temp_conf_file.close()
-#     process = subprocess.Popen(
-#         [
-#             "./bin/FedScale-distributed-party",
-#             # takes 2 arg: config file path, client id
-#             temp_conf_file.name,
-#             str(client_id),
-#         ],
-#         stdout=subprocess.PIPE, 
-#         stderr=subprocess.PIPE,
-#     )
-#     stdout, stderr = process.communicate()
-#     returncode = process.returncode
-#     logging.info(f"{cl.get_task_id()[:6]} - Client[{client_id}] - Subprocess finished with return code {returncode}")
-#     # as soon as one client finishes, all clients should have finished
-#     print('start running!')
-#     if client_id == 0:
-#         cl.send_variable("client_finished", returncode, server_in_list)
-#     log = filter_log_from_fedscale_output(stdout.decode() + stderr.decode())
-#     cl.create_entry(f"{UNIFED_TASK_DIR}:{cl.get_task_id()}:log", log)
-#     os.unlink(temp_conf_file.name)
-#     return json.dumps({
-#         "server_ip": server_ip,
-#         "stdout": stdout.decode(),
-#         "stderr": stderr.decode(),
-#         "returncode": returncode,
-#     })
-
-
 import os
 import json
 import sys
@@ -182,6 +9,8 @@ import datetime
 import yaml
 import socket
 import pickle
+import flbenchmark.datasets
+import flbenchmark.logging
 
 import colink as CL
 
@@ -213,7 +42,7 @@ def config_to_FedScale_format(origin_json_conf):
     json_conf["model"] = origin_json_conf["model"]
     json_conf["bench_param"] = {"mode": "local","device": "gpu"}
     json_conf["training_param"] = origin_json_conf["training"]
-    json_conf["data_dir"] = "/home/haoyukim/data"
+    json_conf["data_dir"] = "~/flbenchmark.working/data"
     return json_conf
 
 def load_json_conf(json_file):
@@ -222,7 +51,7 @@ def load_json_conf(json_file):
     return data
 
 def process_cmd_server(json_conf, local=False):
-    yaml_conf = {'ps_ip': 'localhost', 'ps_port': 29664, 'worker_ips': ['localhost:[2]'], 'exp_path': '~/colink-unifed-fedscale/FedScale/fedscale/cloud', 'executor_entry': 'execution/executor.py', 'aggregator_entry': 'aggregation/aggregator.py', 'auth': {'ssh_user': '', 'ssh_private_key': '~/.ssh/id_rsa'}, 'setup_commands': ['source $HOME/anaconda3/bin/activate fedscale'], 'job_conf': [{'job_name': 'BASE'}, {'seed': 1}, {'log_path': './benchmark'}, {'task': 'simple'}, {'num_participants': 2}, {'data_set': 'breast_horizontal'}, {'data_dir': '../data/csv_data/breast_horizontal'}, {'model': 'logistic_regression'}, {'gradient_policy': 'fed-avg'}, {'eval_interval': 5}, {'rounds': 6}, {'filter_less': 1}, {'num_loaders': 2}, {'local_steps': 5}, {'inner_step': 1}, {'learning_rate': 0.01}, {'batch_size': 32}, {'test_bsz': 32}, {'use_cuda': False}]}
+    yaml_conf = {'ps_ip': 'localhost', 'ps_port': 29664, 'worker_ips': ['localhost:[2]'], 'exp_path': '~/colink-unifed-fedscale/FedScale/fedscale/cloud', 'executor_entry': 'execution/executor.py', 'aggregator_entry': 'aggregation/aggregator.py', 'auth': {'ssh_user': '', 'ssh_private_key': '~/.ssh/id_rsa'}, 'setup_commands': ['source $HOME/anaconda3/bin/activate fedscale'], 'job_conf': [{'job_name': 'BASE'}, {'seed': 1}, {'log_path': './benchmark'}, {'task': 'simple'}, {'num_participants': 2}, {'data_set': 'breast_horizontal'}, {'data_dir': '~/flbenchmark.working/data/csv_data/breast_horizontal'}, {'model': 'logistic_regression'}, {'gradient_policy': 'fed-avg'}, {'eval_interval': 5}, {'rounds': 6}, {'filter_less': 1}, {'num_loaders': 2}, {'local_steps': 5}, {'inner_step': 1}, {'learning_rate': 0.01}, {'batch_size': 32}, {'test_bsz': 32}, {'use_cuda': False}]}
 
     print("process_cmd_server start")
     use_container = "default"
@@ -331,7 +160,7 @@ def process_cmd_client(participant_id, json_conf, time_stamp, temp_output_filena
     time.sleep(10)
     ps_name = f"fedscale-aggr-{time_stamp}"
 
-    yaml_conf = {'ps_ip': 'localhost', 'ps_port': 29664, 'worker_ips': ['localhost:[2]'], 'exp_path': '~/colink-unifed-fedscale/FedScale/fedscale/cloud', 'executor_entry': 'execution/executor.py', 'aggregator_entry': 'aggregation/aggregator.py', 'auth': {'ssh_user': '', 'ssh_private_key': '~/.ssh/id_rsa'}, 'setup_commands': ['source $HOME/anaconda3/bin/activate fedscale'], 'job_conf': [{'job_name': 'BASE'}, {'seed': 1}, {'log_path': './benchmark'}, {'task': 'simple'}, {'num_participants': 2}, {'data_set': 'breast_horizontal'}, {'data_dir': '../data/csv_data/breast_horizontal'}, {'model': 'logistic_regression'}, {'gradient_policy': 'fed-avg'}, {'eval_interval': 5}, {'rounds': 6}, {'filter_less': 1}, {'num_loaders': 2}, {'local_steps': 5}, {'inner_step': 1}, {'learning_rate': 0.01}, {'batch_size': 32}, {'test_bsz': 32}, {'use_cuda': False}]}
+    yaml_conf = {'ps_ip': 'localhost', 'ps_port': 29664, 'worker_ips': ['localhost:[2]'], 'exp_path': '~/colink-unifed-fedscale/FedScale/fedscale/cloud', 'executor_entry': 'execution/executor.py', 'aggregator_entry': 'aggregation/aggregator.py', 'auth': {'ssh_user': '', 'ssh_private_key': '~/.ssh/id_rsa'}, 'setup_commands': ['source $HOME/anaconda3/bin/activate fedscale'], 'job_conf': [{'job_name': 'BASE'}, {'seed': 1}, {'log_path': './benchmark'}, {'task': 'simple'}, {'num_participants': 2}, {'data_set': 'breast_horizontal'}, {'data_dir': '~/flbenchmark.working/data/csv_data/breast_horizontal'}, {'model': 'logistic_regression'}, {'gradient_policy': 'fed-avg'}, {'eval_interval': 5}, {'rounds': 6}, {'filter_less': 1}, {'num_loaders': 2}, {'local_steps': 5}, {'inner_step': 1}, {'learning_rate': 0.01}, {'batch_size': 32}, {'test_bsz': 32}, {'use_cuda': False}]}
 
     if 'use_container' in yaml_conf:
         if yaml_conf['use_container'] == "docker":
