3680bfb9c681688b109f296650e15e4c96bd24c4
xmhuangzhen <xmhuangzhen@sjtu.edu.cn>
2023-05-09 13:36:55 +0800

    test v7 update

--- src/unifed/frameworks/fedscale/protocol.py
+++ src/unifed/frameworks/fedscale/protocol.py
@@ -282,7 +282,7 @@ def run_server(cl: CL.CoLink, param: bytes, participants: List[CL.Participant]):
 
     cl.send_variable("time_stamp", json.dumps(time_stamp), [p for p in participants if p.role == "client"])
 
-    process = subprocess.Popen(f'FEDSCALE_HOME=$(pwd)/FedScale && echo $FEDSCALE_HOME && {ps_cmd}',shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen(f'FEDSCALE_HOME=$(pwd)/FedScale && echo $FEDSCALE_HOME && ps aux | grep feds',shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     stdout, stderr = process.communicate()
     returncode = process.returncode
@@ -328,7 +328,7 @@ def run_client(cl: CL.CoLink, param: bytes, participants: List[CL.Participant]):
 
     ps_cmd = process_cmd_client(participant_id, Config, time_stamp, server_ip)
 
-    process = subprocess.Popen(f'FEDSCALE_HOME=$(pwd)/FedScale && echo $FEDSCALE_HOME && {ps_cmd}',shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen(f'FEDSCALE_HOME=$(pwd)/FedScale && echo $FEDSCALE_HOME && ps aux | grep feds',shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     stdout, stderr = process.communicate()
     returncode = process.returncode
